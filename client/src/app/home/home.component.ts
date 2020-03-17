f8e3182fcc4eb7ba40757d3aef3583876e9edda8
Velibor <veliborvasiljevic7@gmail.com>
2020-03-17 12:33:57 +0100

    we gettin there

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.sass"]
 })
 export class HomeComponent implements OnInit {
-  isMenuShown = false;
+  isMenuShown = true;
   constructor() {}
 
   ngOnInit(): void {}
