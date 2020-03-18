364e750b442ab57bfb6f34d48ab419f74bb64367
Velibor <veliborvasiljevic7@gmail.com>
2020-03-18 17:15:59 +0100

    switchin

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { GalleryComponent } from "./gallery/gallery.component";
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent }
+  { path: "about", component: AboutComponent },
+  { path: "portfolio", component: PortfolioComponent },
+  { path: "gallery", component: GalleryComponent }
 ];
 
 @NgModule({
