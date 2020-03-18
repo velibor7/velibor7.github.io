525f755a73f052b9fda64275392b923d1c4d1441
Velibor <veliborvasiljevic7@gmail.com>
2020-03-18 12:25:27 +0100

    showup done

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.sass']
+  selector: "app-about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.sass"],
+  animations: [
+    trigger("popOverState", [
+      state(
+        "show",
+        style({
+          opacity: 1
+        })
+      ),
+      state(
+        "hide",
+        style({
+          opacity: 0
+        })
+      ),
+      transition("show => hide", animate("400ms ease-out")),
+      transition("hide => show", animate("400ms ease-in"))
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
+  show = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  //this determines
+  // da li ovaj geter sam od sebe moze tamo da se pozove wtf?!
+  get stateName() {
+    return this.show ? "show" : "hide";
   }
 
+  toggle() {
+    this.show = !this.show;
+  }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
