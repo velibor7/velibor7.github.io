abc9cc8f227bbdf9a359d82841dcec1b2cd7b4e7
Velibor <veliborvasiljevic7@gmail.com>
2020-03-18 15:46:12 +0100

    omfg i love angular and everything is working

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,21 +1,87 @@
 import { Component, OnInit } from "@angular/core";
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from "@angular/animations";
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.sass"]
+  styleUrls: ["./home.component.sass"],
+  animations: [
+    trigger("showMenu", [
+      state(
+        "show",
+        style({
+          opacity: 1
+        })
+      ),
+      state(
+        "hide",
+        style({
+          opacity: 0
+        })
+      ),
+      transition("show => hide", animate("400ms ease-out")),
+      transition("hide => show", animate("400ms ease-in"))
+    ]),
+
+    trigger("topBlur", [
+      state(
+        "show",
+        style({
+          // opacity: 1,
+          filter: "blur(5px)"
+        })
+      ),
+      state(
+        "hide",
+        style({
+          // opacity: 0,
+          filter: "blur(0px)"
+        })
+      ),
+      transition("show => hide", animate("400ms ease-out")),
+      transition("hide => show", animate("400ms ease-in"))
+    ]),
+    trigger("arrow", [
+      state(
+        "show",
+        style({
+          transform: "rotateZ(-180deg)"
+        })
+      ),
+      state(
+        "hide",
+        style({
+          transform: "rotateZ(0deg)"
+        })
+      ),
+      transition("show => hide", animate("400ms ease")),
+      transition("hide => show", animate("400ms ease"))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
-  isMenuShown = true;
+  isMenuShown = false;
   constructor() {}
 
   ngOnInit(): void {}
 
-  showMenu() {
+  get menuState() {
+    return this.isMenuShown ? "show" : "hide";
+  }
+
+  menuToggle() {
     this.isMenuShown = !this.isMenuShown;
     // console.log(this.isMenuShown);
   }
-
+  logAnimation($event) {
+    console.log(`${this.isMenuShown} animation ${$event.phaseName}`);
+  }
+  /*
   rotatingClasses() {
     let myClasses = {
       active: this.isMenuShown == true,
@@ -39,4 +105,5 @@ export class HomeComponent implements OnInit {
     };
     return myClasses;
   }
+  */
 }
